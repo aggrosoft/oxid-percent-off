fb7ec15edad7fd8ca37a05706922984febc16e1d
alexander <coding@aggrosoft.de>
2020-07-28 15:00:27 +0200

    percent formula was wrong

--- Application/Model/PercentOffArticle.php
+++ Application/Model/PercentOffArticle.php
@@ -7,8 +7,8 @@ class PercentOffArticle extends PercentOffArticle_parent
     public function getPercentOff()
     {
         if ($this->oxarticles__oxtprice->value){
-            $factor = ($this->oxarticles__oxtprice->value / $this->oxarticles__oxprice->value) - 1;
-            return round($factor * 100) . '%';
+            $factor = ($this->oxarticles__oxprice->value / $this->oxarticles__oxtprice->value) - 1 ;
+            return round($factor * -100) . '%';
         }
         return false;
     }
